d090d57b4ec761c776971215a9f3bab5aeee5bb9
Jagonsac <gonzasacjaime@gmail.com>
2023-03-12 17:18:02 +0100

    Fixed styles and added animations for the homepage

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function() {
-    var element = document.querySelector('.element');
-    if (window.scrollY > 500) {
+    var element = document.querySelector('.maincontent');
+    if (window.scrollY > 10) {
       main-content.classList.add('animate');
     } else {
       main-content.classList.remove('animate');
